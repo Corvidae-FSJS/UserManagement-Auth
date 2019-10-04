5d22c5f7ae79cc3951193bc6e057fa125905c36a
Dylan Corvidae <dylancorvidae@gmail.com>
2019-10-03 17:35:06 -0700

    rewrote delete on ms.js

--- e2e/__tests__/bands.test.js
+++ e2e/__tests__/bands.test.js
@@ -25,10 +25,32 @@ describe('bands api', () => {
   // function postBand(band) {
   //   return request
   //     .post('/api/bands')
+  //     .set('Authorization', user.token)
   //     .send(band)
   //     .expect(200)
-  //     .then(({ body }) => body);
-  // }
+  //     .then(({ body }) => {
+  //       expect(body.owner).toBe(user._id);
+  //       expect(body).toMatchInlineSnapshot(
+  //         {
+  //           _id: expect.any(String),
+  //           owner: expect.any(String)
+  //         },
+  //         `
+  //         Object {
+  //           "__v": 0,
+  //           "_id": Any<String>,
+  //           "genre": "Blackened Crust",
+  //           "guitarists": 1,
+  //           "language": "English",
+  //           "name": "Dödsrit",
+  //           "owner": Any<String>,
+  //           "synths": false,
+  //           "vocals": "whispers, screams",
+  //         }
+  //       `
+  //       );
+  //     });
+  // } 
 
   it('post a band for this user', () => {
     return request
@@ -63,8 +85,6 @@ describe('bands api', () => {
   // it('posts a band', () => {
   //   return postBand(dödsrit)
   //     .then(band => {
-  //       console.log(dödsrit);
-
   //       expect(band).toEqual({
   //         _id: expect.any(String),
   //         __v: 0,
@@ -74,7 +94,9 @@ describe('bands api', () => {
   // });
 
   // it('gets a band by id', () => {
-  //   return postBand(dödsrit)
+  //   return request
+  //     .post('/api/bands')
+  //     .set('Authorization', user.token)
   //     .then(band => {
   //       return request.get(`/api/bands/${band._id}`)
   //         .expect(200)
