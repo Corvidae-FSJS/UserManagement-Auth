7c7efd84b9ab26022a29273dd20b9c27a525d1a1
Dylan Corvidae <dylancorvidae@gmail.com>
2019-10-04 14:10:13 -0700

    finished and passing all tests

--- e2e/__tests__/me.test.js
+++ e2e/__tests__/me.test.js
@@ -1,4 +1,4 @@
-const request = require();
+const request = require('../request');
 const { dropCollection } = require('../db');
 const { signupUser } = require('../data-helpers');
 const mongoose = require('mongoose');
@@ -9,8 +9,7 @@ describe('Band API', () => {
 
   let user = null;
   beforeEach(() => {
-    return signupUser()
-      .then(newUser => (user = newUser));
+    return signupUser().then(newUser => (user = newUser));
   });
 
   const band = {
@@ -20,7 +19,7 @@ describe('Band API', () => {
     guitarists: 1,
     vocals: 'whispers, screams',
     synths: false,
-    language: 'English'
+    language: 'Swedish'
   };
 
   function postBand(band) {
@@ -56,6 +55,46 @@ describe('Band API', () => {
           });
       });
   });
-  
-  
+
+  it('gets a favorited band', () => {
+    return putBand(band)
+      .then(() => {
+        return request
+          .get(`/api/me/favorites`)
+          .set('Authorization', user.token)
+          .expect(200)
+          .then(({ body }) => {
+            expect(body[0]).toMatchInlineSnapshot(
+              {
+                _id: expect.any(String)
+              },
+              `
+            Object {
+              "_id": Any<String>,
+              "name": "Dödsrit",
+            }
+          `
+            );
+          });
+      });
+  });
+
+  it('deletes a favorited band', () => {
+    return putBand(band)
+      .then(favoritedBand => {
+        return request
+          .delete(`/api/me/favorites/${favoritedBand[0]}`)
+          .set('Authorization', user.token)
+          .expect(200)
+          .then(() => {
+            return request
+              .get('/api/me/favorites')
+              .set('Authorization', user.token)
+              .expect(200)
+              .then(({ body }) => {
+                expect(body.length).toBe(0);
+              });
+          });
+      });
+  });
 });
